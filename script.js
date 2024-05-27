edb2d57525c2f358a904d061cfce4fb61658204d
IMAMRAJ22 <imamraj2003@gmail.com>
2024-05-27 11:25:14 +0530

    file is modified.

--- script.js
+++ script.js
@@ -18,14 +18,23 @@ cancel.addEventListener("click", function(event){
 //select container
 var container=document.querySelector(".container")
 var addbook=document.getElementById("add-book")
-var book-title=document.getElementById("book-title")
-var book-author=document.getElementById("book-author")
+var booktitle=document.getElementById("book-title")
+var bookauthor=document.getElementById("book-author")
 var description=document.getElementById("book-des")
 
 addbook.addEventListener("click", function(event){
     event.preventDefault()
     var div=document.createElement("div")
-    div.setAttribute("class", "container")
-    div.innerHTML='<h1>${book-title.value}</h1>'
-    
+    div.setAttribute("class", "books")
+    div.innerHTML=`<h2>${booktitle.value}</h2>
+    <h5>${bookauthor.value}</h5>
+    <p>${description.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`   //(`)left corner
+    container.append(div)
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
 })
+
+function deletebook(event){
+    event.target.parentElement.remove()
+}
